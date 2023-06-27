f0de98ddd321d99d71677b0fde6c3148201159c0
ChrisColeTech <moneymagnet216@gmail.com>
2023-06-26 22:47:12 -0400

    fixed image and code cleanup

--- Widget/src/app/app.component.ts
+++ Widget/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { ChangeDetectorRef, Component, HostListener, ViewChild } from '@angular/core';
-import { MatDrawerMode, MatSidenav } from '@angular/material/sidenav';
-import { BreakpointObserver, Breakpoints, MediaMatcher } from '@angular/cdk/layout';
-import { Router, NavigationEnd } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,32 +6,5 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  @ViewChild('sidenav') sidenav: MatSidenav;
-  isSidenavOpen = false;
-  isSidenavFixed = true;
-  sidenavMode: MatDrawerMode = 'side'; // Update the type here
-  mobileQuery: MediaQueryList;
 
-  private _mobileQueryListener: () => void;
-
-  constructor(
-    private changeDetectorRef: ChangeDetectorRef,
-    private media: MediaMatcher,
-    private router: Router
-  ) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-
-    // Subscribe to router events
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.closeSidenav();
-      }
-    });
-  }
-
-  closeSidenav() {
-    this.sidenav.close();
-  }
 }
