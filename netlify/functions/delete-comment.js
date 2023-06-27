6640a05e02b1ce0e089b8f6b59632f94ced0da5a
ChrisColeTech <ChrisColeTech@users.noreply.github.com>
2023-06-27 22:21:12 +0000

    deploy: 64ef4e4b174d5ef7d653de29ba16f2b049dfb722

--- netlify/functions/delete-comment.js
+++ netlify/functions/delete-comment.js
@@ -4,10 +4,11 @@ const { Client, query } = require('faunadb')
 const client = new Client({ secret: 'fnAFG-Ky5LAATX9wNckFUbX0ngbxY2jv_PlqSUVN' });
 
 const handler = async (event) => {
-  const { id } = event
-  console.log(`Function 'delete' invoked. delete id: ${id}`)
+  const data = JSON.parse(event.body);
+  const { ref } = data;
+  console.log(`Function 'delete' invoked. delete ref: ${ref}`)
   try {
-    const response = await client.query(query.Delete(query.Ref(query.Collection('comments'), id)))
+    const response = await client.query(query.Delete(query.Ref(query.Collection('comments'), ref)))
     console.log('success', response)
     return {
       statusCode: 200,
