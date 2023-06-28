236e15486f1632d7e46437fb6e7956adc4bc9a45
ChrisColeTech <ChrisColeTech@users.noreply.github.com>
2023-06-28 00:08:30 +0000

    deploy: ef70e060c89ad5234028108a405ff7f56ab76d4b

--- netlify/functions/delete.js
+++ netlify/functions/delete.js
@@ -4,18 +4,13 @@ const { Client, query } = require('faunadb')
 const client = new Client({ secret: 'fnAFG-Ky5LAATX9wNckFUbX0ngbxY2jv_PlqSUVN' });
 
 const handler = async (event) => {
-  const data = JSON.parse(event.body);
-  const { ref } = data;
-  console.log(`Function 'delete' invoked. delete ref: ${ref}`)
+  const { id } = event
+  console.log(`Function 'delete' invoked. delete id: ${id}`)
   try {
-    const response = await client.query(query.Delete(query.Ref(query.Collection('comments'), ref)))
+    const response = await client.query(query.Delete(query.Ref(query.Collection('comments'), id)))
     console.log('success', response)
     return {
       statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*', // Allow requests from any origin
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-      },
       body: JSON.stringify(response),
     }
   } catch (error) {
