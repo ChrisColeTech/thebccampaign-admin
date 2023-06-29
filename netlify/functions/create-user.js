2810fbcdfefe5793433ac4356bd28f47f237c3ea
ChrisColeTech <ChrisColeTech@users.noreply.github.com>
2023-06-29 12:53:06 +0000

    deploy: 234d2585c6cdddc60cd8c33f1fae28bc892fdb5c

---
+++ netlify/functions/create-user.js
@@ -0,0 +1,35 @@
+const { Client, query } = require('faunadb');
+const { handleOptions } = require('./handleOptions');
+/* Configure faunaDB Client with our secret */
+const client = new Client({ secret: 'fnAFG-Ky5LAATX9wNckFUbX0ngbxY2jv_PlqSUVN' });
+
+const createUser = async (event) => {
+    if (event.httpMethod === "OPTIONS") {
+        return handleOptions(event);
+    }
+
+    const data = JSON.parse(event.body);
+    console.log('Function `createUser` invoked', data);
+    // Logic to create a user in the database
+
+    try {
+        const response = await client.query(query.Create(query.Collection('users'), item));
+        console.log('Success', response);
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*', // Allow requests from any origin
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+            },
+            body: JSON.stringify(response),
+        };
+    } catch (error) {
+        console.log('Error', error);
+        return {
+            statusCode: 400,
+            body: JSON.stringify(error),
+        };
+    }
+};
+
+module.exports = { createUser };
